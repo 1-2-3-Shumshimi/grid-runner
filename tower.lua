7981ccee82c560adbe0c5315f4027dfe2ee1f047
Jeffrey Gu <jeffreygu.17@gmail.com>
2017-08-01 22:37:10 -0500

    tower mockup, grids marked upon tower creation

--- tower.lua
+++ tower.lua
@@ -5,12 +5,37 @@ local tower = {}
   -- make a object-specific draw function
   
 
+  -- gameplay characteristics
   tower.attackSpeed = 1
   tower.damage = 2
   tower.range = 4
-  tower.areaOfEffect = 1
-  tower.needsUpdate = false -- since towers are stationary, won't change unless upgrade
   
+  -- map placement characteristics
+  tower.size = 1
+  
+  tower.x = 0 -- upper left side coordinates
+  tower.y = 0
+  tower.needsUpdate = false -- towers are stationary, won't change after initial draw unless upgrade
+
+  
+  -- create a new creep object
+  -- either pass in a table with {} or set it later
+  function tower:new(object)
+    object = object or {attackSpeed = tower.attackSpeed, damage = tower.damage, range = tower.range, size = tower.size}
+    
+    object.needsUpdate = true
+    
+    setmetatable(object, self)
+    self.__index = self
+    return object
+    
+  end
+  
+  -- these are inverted from pathfinder module ... ?
+  function tower:setCoord(x, y) 
+    self.x = x
+    self.y = y
+  end
   
   function tower:draw()
     -- redraw all towers on map
