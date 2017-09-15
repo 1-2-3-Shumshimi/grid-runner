aed24e534a5c21cdefdd09dbe53641f93493aac7
Jeffrey Gu <jeffreygu.17@gmail.com>
2017-09-14 20:24:04 -0500

    Created player class skeleton

---
+++ player.lua
@@ -0,0 +1,23 @@
+player = class {
+  init = function(self, HP, currency, name, towerSkin, creepSkin, defenseRating)
+    self.HP = HP  -- TODO: determine standard (HP = 100?)
+    self.name = name
+    self.towerSkin = towerSkin
+    self.creepSkin = creepSkin
+    self.currency = currency
+    self.defenseRating = defenseRating -- resistance to creep damage
+  end
+ 
+  function player:hasLost()
+    return self.HP <= 0
+  end
+  
+  function player:toString()
+    return "Player "..self.name..", HP = "..self.HP..", Currency = "..self.currency
+  end
+  
+  function player:generateID()
+    -- TODO: randomly generate ID for easier comparison (e.g. ID'ing creeps and towers)
+  end
+  
+}
