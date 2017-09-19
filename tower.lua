0121e84c0c91c2e91cd4ec5c10033e21927c0a2c
Jeffrey Gu <jeffreygu.17@gmail.com>
2017-09-18 21:17:25 -0500

    base player file added

--- tower.lua
+++ tower.lua
@@ -27,48 +27,6 @@ tower = Class{
     self.direction = "left"
   end
 }
-
-
---local tower = {}
---  -- if it belongs to the indv object, do object:function()
---  -- if it's static, do object.function()
-  
---  -- make a object-specific draw function
-  
-
---  -- gameplay characteristics
---  tower.attackSpeed = 1
---  tower.damage = 2
---  tower.range = 4
---  tower.attackCapacity = 1  -- i.e. # of enemies it can attack simultaneously
---  tower.attackOccupancy = 0 -- i.e. # of enemies it is currently attacking
-  
---  tower.hasFired = false
---  tower.lastFired = 0
-  
---  -- map placement characteristics
---  tower.size = 1
-  
---  tower.x = 0 -- upper left side coordinates
---  tower.y = 0
---  tower.needsUpdate = false -- towers are stationary, won't change after initial draw unless upgrade
-
-  
---  -- create a new creep object
---  -- either pass in a table with {} or set it later
---  function tower:new(object)
---    object = object or {attackSpeed = tower.attackSpeed, damage = tower.damage, range = tower.range, attackCapacity = tower.attackCapacity, size = tower.size}
-    
---    object.attackOccupancy = 0
---    object.needsUpdate = true
---    object.hasFired = false
---    object.lastFired = 0
-    
---    setmetatable(object, self)
---    self.__index = self
---    return object
-    
---  end
   
   -- these are inverted from pathfinder module ... ?
   function tower:setCoord(x, y) 
