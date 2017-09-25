a6caa67bd817bbc6a4c1c188bbf8516aa73e2bf6
Jonathan Shieh <jshieh130@gmail.com>
2017-09-25 18:01:49 -0500

    added the gamestate switch to the server POC (forgot from last commit)

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ class = require('hump.class')
 grid = require('jumper.grid') --https://github.com/Yonaba/Jumper.git
 pathfinder = require('jumper.pathfinder')
 anim8 = require('anim8.anim8')
+socket = require('socket')
 
 --utilities and entities--
 utils = require('utils')
@@ -15,8 +16,13 @@ player = require('player')
 
 --game states--
 game = require('game')
+client = require('server/client') --jonathan: just POC
 
 function love.load()
     gamestate.registerEvents()
-    gamestate.switch(game)
+--    gamestate.switch(game) --jonathan: uncomment this to play the game as is
+    print("switch to client") --jonathan: entry point to client/server POC
+    gamestate.switch(client)
+    --jonathan: remember that prior to running the client you have to first start up the server
+    -- which can be done with the command "lua server/server.lua" from the directory of grid-runner
 end
